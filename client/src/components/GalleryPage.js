0a1181b00db3efbcb700fdf2551eba504f4bc6ab
Oscar Leal <oscarleal234@gmail.com>
2023-10-05 11:10:32 -0500

    added animation

--- client/src/components/GalleryPage.js
+++ client/src/components/GalleryPage.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 import Nav from './Nav';
 import galleryData from '../gallerydata';
+import gsap from 'gsap';
 
 // import JP_BG from './Cherry-Blossoms.svg';
 const GalleryPage = () => {
@@ -13,14 +14,32 @@ const GalleryPage = () => {
   );
 
   console.log(country.img1);
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    const images = document.querySelectorAll('img');
+
+    gsap.set(images, { autoAlpha: 1 });
+
+    gsap.from(images, {
+      autoAlpha: 0, // Start with 0 opacity
+      duration: 2, // Animation duration
+    });
+  }, [id, key]);
+
+  useEffect(() => {
+    // Update the key whenever id changes
+    setKey((prevKey) => prevKey + 1);
+  }, [id]); // Include id in the dependency array
+
   return (
     <div
       className="min-h-screen bg-cover bg-no-repeat bg-center"
       style={{ backgroundImage: `url(${country.bg})` }}
     >
       <Nav />
-      <div className="p-4">
-        <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="p-4 ">
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-4 images">
           <div>
             <img
               class="h-auto max-w-full rounded-lg"
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import { GiWorld } from 'react-icons/gi';
+
 const Nav = () => {
   return (
     <div>
-      <nav>
-        <img src="./Globe.png" className="nav--img" />
-        <h2>O's Travel Journal</h2>
+      <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="/" class="flex items-center">
+            <GiWorld className="text-3xl text-white m-4" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              {' '}
+              <h2>O's Travel Journal</h2>
+            </span>
+          </a>
+        </div>
       </nav>
     </div>
   );
