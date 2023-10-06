13f34285d65e8a5368dbdd6bbf0d312ae70c7733
Oscar Leal <oscarleal234@gmail.com>
2023-10-05 19:56:23 -0500

    added instructions

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,37 +8,41 @@ const Card = () => {
   return (
     <div>
       <Nav />
+      <h1 className="flex justify-center flex-wrap text-center text-4xl text-black font-bold mt-6">
+        Click on the Country's name to see some of my favorite photos from each
+        trip.
+      </h1>
       <div className="card-container">
         {travelData.map((country) => (
-          <Link to={`gallery/${country.id}`} key={country.id}>
-            <div
-              href="#"
-              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w3xl ="
-            >
-              <img
-                className="object-cover rounded-t-lg  md:h-auto md:w-64 md:rounded-none md:rounded-l-lg"
-                src={country.imageUrl}
-                style={{ height: '100%', maxWidth: '100%' }}
-                alt=""
-              />
-              <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-9 text-lg font-semibold tracking-tight text-gray-600">
-                  <a href={country.googleMapsUrl}>
-                    <IoLocationSharp className="text-3xl text-red-500" /> View
-                    on Google Maps
-                  </a>
-                </h5>
+          <div
+            href="#"
+            className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w3xl ="
+          >
+            <img
+              className="object-cover rounded-t-lg  md:h-auto md:w-64 md:rounded-none md:rounded-l-lg"
+              src={country.imageUrl}
+              style={{ height: '100%', maxWidth: '100%' }}
+              alt=""
+            />
+            <div className="flex flex-col justify-between p-4 leading-normal">
+              <h5 className="mb-9 text-lg font-semibold tracking-tight text-gray-600">
+                <a href={country.googleMapsUrl}>
+                  <IoLocationSharp className="text-3xl text-red-500" /> View on
+                  Google Maps
+                </a>
+              </h5>
+              <Link to={`gallery/${country.id}`} key={country.id}>
                 <h1 className="font-bold text-3xl mb-4">{country.country} </h1>
-                <h6 className="text-lg">
-                  {country.startDate} - {country.endDate}
-                </h6>
-                <h6 className="text-2xl mt-4">Age: {country.age}</h6>
-                <p className="mb-3 font-semibold text-lg text-gray-700 mt-5">
-                  {country.description}
-                </p>
-              </div>
+              </Link>
+              <h6 className="text-lg">
+                {country.startDate} - {country.endDate}
+              </h6>
+              <h6 className="text-2xl mt-4">Age: {country.age}</h6>
+              <p className="mb-3 font-semibold text-lg text-gray-700 mt-5">
+                {country.description}
+              </p>
             </div>
-          </Link>
+          </div>
         ))}
       </div>
     </div>
